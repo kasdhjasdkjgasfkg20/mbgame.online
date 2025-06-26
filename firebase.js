ee5e8ee090638a57abbd53defe789f22ff233324
kjhdiauiyoruwqhfjuisahguiafkwqhgifjasasd <mbbankgame.online@gmail.com>
2025-06-26 17:32:14 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,12 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBAhJbI0ydZDGNGa1fCJZE4zYxWUZuyXL8",
-    authDomain: "chatbot-support-e1774.firebaseapp.com",
-    projectId: "chatbot-support-e1774",
-    messagingSenderId: "913372631582",
-    appId: "1:913372631582:web:598a0740593bc063c5940f"
-};
+   const firebaseConfig = {
+            apiKey: "AIzaSyBAhJbI0ydZDGNGa1fCJZE4zYxWUZuyXL8",
+            authDomain: "chatbot-support-e1774.firebaseapp.com",
+            databaseURL: "https://chatbot-support-e1774-default-rtdb.asia-southeast1.firebasedatabase.app",
+            projectId: "chatbot-support-e1774",
+            storageBucket: "chatbot-support-e1774.firebasestorage.app",
+            messagingSenderId: "913372631582",
+            appId: "1:913372631582:web:598a0740593bc063c5940f",
+            measurementId: "G-VG2WPPRL16"
+        };
 
 firebase.initializeApp(firebaseConfig);
